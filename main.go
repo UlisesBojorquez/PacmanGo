547e6f7d2ea8006e3607731a027cc8defb81aac1
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-06-05 11:55:54 -0700

    Validate enemies selection input

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	_ "image/png"
 	"log"
+	"os"
 
 	"github.com/UlisesBojorquez/PacmanGo/pacman"
 	"github.com/hajimehoshi/ebiten"
 )
 
 func main() {
-	g := pacman.NewGame() //create new game, pacman is the dir and NewGame is in game.go
+	enemiesNumber := flag.Int("enemies", 4, "an int")
+	flag.Parse()
+
+	if *enemiesNumber < 1 {
+		fmt.Println("Few Enemies: Select more than 0 and less than 5 enemies")
+		os.Exit(0)
+
+	} else if *enemiesNumber > 4 {
+		fmt.Println("Too much enemies: Select more than 0 and less than 5 enemies")
+		os.Exit(0)
+	}
+
+	g := pacman.NewGame(*enemiesNumber) //create new game, pacman is the dir and NewGame is in game.go
 
 	if err := ebiten.Run(g.Update, g.ScreenWidth(), g.ScreenHeight(), 1, "Pacman"); err != nil { //2
 		log.Fatal(err)
