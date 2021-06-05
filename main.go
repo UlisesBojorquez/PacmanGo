c945da208e141314b4504dc48d48068f6469908a
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-06-05 11:56:56 -0700

    Delete commented code

--- main.go
+++ main.go
@@ -30,36 +30,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-/*
-package main
-
-import (
-	"log"
-
-	"github.com/UlisesBojorquez/pacman"
-	"github.com/hajimehoshi/ebiten/v2"
-)
-
-type Game struct{} //Implements ebitent.Game for the ebiten interface
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
-}
-
-func main() {
-	/*ebiten.SetWindowSize(640, 480)                  //assign the size of thr screen
-	ebiten.SetWindowTitle("Hello, World!")          //assign the title
-	if err := ebiten.RunGame(&Game{}); err != nil { //run the game
-		log.Fatal(err)
-	}
-
-}*/
